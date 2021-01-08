8b07b1591b896c2906a9913e1b3a8d764af94b1e
Алексей Жуков <gkvalex7@gmail.com>
2021-01-08 20:35:36 +0300

    client login finished

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -9,7 +9,27 @@ export const Login = () => {
 
     const startLogin = () => {
         initLogin(true);
+        setTimeout(() => {
+            document.querySelector('.sb-login__auth').classList.add('scale-in');
 
+            const inputFields = document.querySelectorAll('input');
+            console.log(inputFields);
+            inputFields.forEach((input) => {
+                input.addEventListener('focus', () => {
+                    input.parentElement.classList.add('focused');
+                });
+
+                input.addEventListener('blur', () => {
+                    const value = input.value;
+                    if (value === '') {
+                        input.classList.remove('filled');
+                        input.parentElement.classList.remove('focused');
+                    } else {
+                        input.classList.add('filled');
+                    }
+                });
+            })
+        }, 100);
     };
 
     return (
@@ -37,15 +57,28 @@ export const Login = () => {
             {
                 loginStarted && (
                     <>
-                    <div className={'sb-login__auth scale-transition scale-out ' + (loginStarted ? 'scale-in' : '')}>
-                        <div className="sb-login__auth-container container">
-                            <div className="row">
-                                <div className="input-field col s12">
-                                    <input type="text" id="login" className="autocomplete" />
-                                    <label htmlFor="login">{ tr('Логин') }</label>
+                    <div className={'sb-login__auth scale-transition scale-out'}>
+                        <div className="sb-login__auth-container">
+                            <div className="sb-login__auth-input">
+                                <label htmlFor="login" className='form-label'>{ tr('Логин') }</label>
+                                <input  type="text" id="login"
+                                        className="form-input input-login"
+                                        autoComplete="off"/>
+                            </div>
+                            <div className="sb-login__auth-input">
+                                <label htmlFor="password" className='form-label'>{ tr('Пароль') }</label>
+                                <input  type="password" id="password"
+                                        className="form-input input-pass"
+                                        autoComplete="off"/>
+                            </div>
+                            <div className="sb-login__auth-submitContainer">
+                                <div className="sb-login__auth-submit enter z-depth-4 btn-floating waves-effect">
+                                    <span className='sb-login__auth-submit-text'>{ tr('Вход') }</span>
+                                </div>
+                                <div className="sb-login__auth-submit register z-depth-4 btn-floating waves-effect">
+                                    <span className='sb-login__auth-submit-text'>{ tr('Регистрация') }</span>
                                 </div>
                             </div>
-
                         </div>
                     </div>
 
